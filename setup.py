6941f9ae8eda457b72dfa267053dec7a6f8996d3
bcaton <bcaton85@gmail.com>
2022-01-26 09:53:30 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='quay',
+      version='3.6',
+      description='Quay application',
+      author='quay team',
+      author_email='',
+      url='https://github.com/bcaton85/quay',
+     )
