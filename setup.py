77db18fbf68a00a43bc81cc7a56f00bb56ca0db7
Brandon Caton <bcaton@redhat.com>
2022-02-07 17:44:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 import os
 
 quay_dir = os.path.dirname(os.path.realpath(__file__))
