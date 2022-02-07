20daa148a2d96bee91a0aef25357b119ae5e3dd1
Brandon Caton <bcaton@redhat.com>
2022-02-07 16:44:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
+import os
+
+quay_dir = os.path.dirname(os.path.realpath(__file__))
+requirementPath = quay_dir + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(name='quay',
       version='3.6',
@@ -7,4 +15,5 @@ setup(name='quay',
       author_email='',
       url='https://github.com/bcaton85/quay',
       packages=['data','data.model'],
+      install_requires=install_requires,
      )
