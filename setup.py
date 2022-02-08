1cda5bd123d374c06df379c6b575d0995ac5c8f1
Brandon Caton <bcaton@redhat.com>
2022-02-08 17:03:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(name='quay',
       author='quay team',
       author_email='',
       url='https://github.com/bcaton85/quay',
-      packages=['data','data.model','data.model.oci','util','util.security','util.metrics','image','image.docker'],
+      packages=['data','data.model','data.model.oci','util','util.security','util.metrics','image','image.docker','digest'],
       install_requires=install_requires,
      )
