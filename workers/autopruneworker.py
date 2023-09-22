9f3dcdc847f37356fd581fa27430e934306b4295
Sunandadadi <Sunandadadi@users.noreply.github.com>
2023-09-22 13:41:14 -0400

    wip: autoprune worker (#4)
    * wip: added selecting tasks from autoprunetask table
    
    * batched auto prune tasks + simplified fetching autoprune policies query + fixing last_ran_ms to get time in milli sec + using one query to fetch never_ran and last_ran auto prune tasks
    
    * added pruning of tags basaed on namespace policy config
    
    * clean up and minor fixes
    
    * fix: use namespace_id to fetch auto prune task
    
    * wip: added selecting tasks from autoprunetask table
    
    * paginating queries + fixes based on review
    
    * remove duplicate logger
    
    * adding db_transaction lock to fetch autoprune tasks
    
    * Adding error handling for worker
    
    * change blocking to one row at a time per worker
    
    * Removing endpoints from endpoints/organization
    
    * minor clean up
    
    * removing db_for_update

--- workers/autopruneworker.py
+++ workers/autopruneworker.py
@@ -4,6 +4,7 @@ import time
 import features
 
 from app import app
+from data.model.autoprune import *
 from workers.gunicorn_worker import GunicornWorker
 from workers.worker import Worker
 from util.log import logfile_path
@@ -11,6 +12,7 @@ from util.log import logfile_path
 
 logger = logging.getLogger(__name__)
 POLL_PERIOD = app.config.get("AUTO_PRUNING_POLL_PERIOD", 30)
+BATCH_SIZE = app.config.get("AUTO_PRUNING_BATCH_SIZE", 10)
 
 
 class AutoPruneWorker(Worker):
@@ -19,8 +21,26 @@ class AutoPruneWorker(Worker):
         self.add_operation(self.prune, POLL_PERIOD)
 
     def prune(self):
-        # TODO: add prune logic here
-        logger.info("starting auto prune logic")
+        for i in range(BATCH_SIZE):
+            try:
+                autoprune_task = fetch_autoprune_task()
+                if not autoprune_task:
+                    return
+
+                policies = get_namespace_autoprune_policies_by_id(autoprune_task.namespace_id)
+                if not policies:
+                    # When implementing repo policies, fetch repo policies before deleting the task
+                    delete_autoprune_task(autoprune_task)
+                    continue
+
+                execute_namespace_polices(policies, autoprune_task.namespace)
+
+                update_autoprune_task(autoprune_task, task_status="success")
+            except Exception as err:
+                update_autoprune_task(autoprune_task, task_status=str(err))
+
+        return
+
 
 def create_gunicorn_worker():
     worker = GunicornWorker(__name__, app, AutoPruneWorker(), features.AUTO_PRUNE)
