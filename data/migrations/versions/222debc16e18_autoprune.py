58542c663da8a8619031f8aeb1e2c3035e70bea1
bcaton <bcaton@redhat.com>
2023-09-25 14:39:54 -0400

    adding indexes to db schema

--- data/migrations/versions/222debc16e18_autoprune.py
+++ data/migrations/versions/222debc16e18_autoprune.py
@@ -19,8 +19,25 @@ def upgrade(op, tables, tester):
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("uuid", sa.String(length=36), nullable=False),
         sa.Column("namespace_id", sa.Integer(), nullable=False),
-        sa.Column("policy", sa.Text(), nullable=True),
+        sa.Column("policy", sa.Text(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["namespace_id"], ["user.id"], name=op.f("fk_namespaceautoprunepolicy_namespace_id_user")
+        ),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_namespaceautoprunepolicyid")),
+    )
+
+    op.create_index(
+        "namespaceautoprunepolicy_namespace_id",
+        "namespaceautoprunepolicy",
+        ["namespace_id"],
+        unique=True,
+    )
+
+    op.create_index(
+        "namespaceautoprunepolicy_uuid",
+        "namespaceautoprunepolicy",
+        ["uuid"],
+        unique=True,
     )
 
     op.create_table(
@@ -29,10 +46,25 @@ def upgrade(op, tables, tester):
         sa.Column("namespace_id", sa.Integer(), nullable=False),
         sa.Column("last_ran_ms", sa.BigInteger(), nullable=True),
         sa.Column("status", sa.Text(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ["namespace_id"], ["user.id"], name=op.f("fk_autoprunetaskstatus_namespace_id_user")
+        ),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_autoprunetaskstatusid")),
     )
 
-    # TODO: add indexes to uuid and last_ran_ms
+    op.create_index(
+        "autoprunetaskstatus_namespace_id",
+        "autoprunetaskstatus",
+        ["namespace_id"],
+        unique=True,
+    )
+
+    op.create_index(
+        "autoprunetaskstatus_last_ran_ms",
+        "autoprunetaskstatus",
+        ["last_ran_ms"],
+        unique=False,
+    )
 
 
 def downgrade(op, tables, tester):
